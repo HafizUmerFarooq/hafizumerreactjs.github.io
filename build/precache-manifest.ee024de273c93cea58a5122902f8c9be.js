457e03aa607137752b545f9df98001f84d79789c
hafizumar9076@gmail.com <hafizumar9076@gmail.com>
2020-08-12 17:23:27 +0500

    test app

---
+++ build/precache-manifest.ee024de273c93cea58a5122902f8c9be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eba470a1a815603c447817875a7f573",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/index.html"
+  },
+  {
+    "revision": "af3e0a9d652d2b40e06a",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/css/2.22db8cda.chunk.css"
+  },
+  {
+    "revision": "af3e0a9d652d2b40e06a",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/js/2.0c468d63.chunk.js"
+  },
+  {
+    "revision": "7c31314e008d1a59ddfae5300ece5508",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/js/2.0c468d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5427f92146998533089",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/js/main.a2ee4a5a.chunk.js"
+  },
+  {
+    "revision": "86e642e400c8e046a41c",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/js/runtime-main.76c0c67c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/HafizUmerFarooq/UmerReactFirstSite.github.io.git/static/media/slick.f97e3bbf.svg"
+  }
+]);
